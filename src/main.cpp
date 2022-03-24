2a9ff311d12d14415768f2a7e966626d99ace42e
Adil Shah <ashah@softwareinstitute.com>
2022-03-24 17:11:15 +0000

    new feature from main branch

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,15 @@
 #include <iostream>
 
+int newfeaturefrommainbranch()
+{
+    std::cout << "hello world" << std::endl;
+    return 0;
+}
+
+
+
+
+
 int main()
 {
     std::cout << "hello world" << std::endl;
